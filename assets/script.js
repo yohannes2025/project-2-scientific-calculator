62553ebdd89839bd1611b596208210b869439a4b
Yohannes Mebrahtu <johannes.m.tekle@gmail.com>
2024-12-08 23:10:06 +0000

    set the value of the innerText of the clicked button to curent value and then display it.

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,17 @@
+//Add event listener for the display and the buttons
 document.addEventListener("DOMContentLoaded", function(){   
     const display = document.getElementById("calc-display");
     const buttons = document.getElementsByClassName("btn");
-        
+    
+    let currentValue = "";
+    
+    //Add click event listener for each button
     for (let i = 0; i <buttons.length; i++){
         const button = buttons[i];
         button.addEventListener("click", function(){
-            console.log("button clicked is: ", button.innerText);
+            const value = button.innerText;
+            currentValue += value;
+            display.value = currentValue;   //set the innerText of the button to current value and then display on the display
         });
         
     }
