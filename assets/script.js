19e9ef0ffca12213dd7a81c10ed4ae5ef0c215bf
Yohannes Mebrahtu <johannes.m.tekle@gmail.com>
2024-12-08 22:54:27 +0000

    add click button event listener for each button

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(){
-    console.log("Document ready");
+document.addEventListener("DOMContentLoaded", function(){   
     const display = document.getElementById("calc-display");
     const buttons = document.getElementsByClassName("btn");
+        
+    for (let i = 0; i <buttons.length; i++){
+        const button = buttons[i];
+        button.addEventListener("click", function(){
+            console.log("button clicked is: ", button.innerText);
+        });
+        
+    }
+    
 });
