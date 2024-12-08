7c2cc11c934749a8236100ed21b50cde94969a92
Yohannes Mebrahtu <johannes.m.tekle@gmail.com>
2024-12-08 21:59:16 +0000

    add event listener for the display and the buttons

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("Document ready");
+    const display = document.getElementById("calc-display");
+    const buttons = document.getElementsByClassName("btn");
+});
